574f0cb1d9175e401fae50a5d778c31bc862275e
RAGAVARCHINI S <133149377+Ragavarchini@users.noreply.github.com>
2025-02-09 14:38:49 +0530

    Time: 8 ms (80.48%) | Memory: 42.2 MB (93.16%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int frq1[] = new int[200];
+        int frq2[] = new int[200];
+        for(int i=0;i<s.length();i++)
+        {
+           if(frq1[s.charAt(i)]!=frq2[t.charAt(i)])
+                 return false;
+            frq1[s.charAt(i)] = i+1;
+            frq2[t.charAt(i)] = i+1;
+
+        }
+        return true;
+    }
+}
